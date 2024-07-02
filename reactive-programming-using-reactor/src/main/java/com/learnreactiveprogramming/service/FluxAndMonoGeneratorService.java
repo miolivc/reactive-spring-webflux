70d08278246f28952b672c122843717c542f8ec7
Michelle Oliveira <michelleolivcosta@gmail.com>
2024-07-02 07:29:28 -0300

    first Flux and Mono examples

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,37 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        /**
+         * ao chamar o .log as informações mais detalhadas sobre a comunicação
+         * entre publisher e subscriber são mostradas
+         * ex.: request, onNext, onComplete, onError
+         */
+
+        return Flux.fromIterable(Arrays.asList("alex", "ben", "chloe"))
+                .log(); // db or remote call
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("alex");
+    }
+
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe((name) -> System.out.println("Name is: " + name));
+
+
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe((name) -> System.out.println("Mono Name is: " + name));
+    }
+
 }
