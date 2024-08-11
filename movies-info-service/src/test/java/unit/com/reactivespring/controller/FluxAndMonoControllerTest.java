3cff1f6e1197c9e9cd4987be38e5283db2a9724d
Michelle Oliveira <michelleolivcosta@gmail.com>
2024-08-11 08:27:31 -0300

    diferentes formas de testar um endpoint reativo

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +21,7 @@ class FluxAndMonoControllerTest {
     private WebTestClient webTestClient;
 
     @Test
-    void flux() {
+    void flux_approach() {
 
         webTestClient
                 .get()
@@ -27,5 +32,38 @@ class FluxAndMonoControllerTest {
                 .expectBodyList(Integer.class)
                 .hasSize(3);
 
+    }
+
+    @Test
+    void flux_approach2() {
+
+        var flux = webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3)
+                .verifyComplete();
+    }
+
+    @Test
+    void flux_approach3() {
+
+        webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    final List<Integer> responseBody = listEntityExchangeResult.getResponseBody();
+                    assert Objects.requireNonNull(responseBody).size() == 3;
+                });
     }
 }
