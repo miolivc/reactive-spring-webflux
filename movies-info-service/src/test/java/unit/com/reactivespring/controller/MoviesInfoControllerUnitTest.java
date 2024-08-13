1da88c967e6245e0608908c423528dcf61961b9b
Michelle Oliveira <michelleolivcosta@gmail.com>
2024-08-12 23:21:27 -0300

    teste unitario para getAllMovieInfo

---
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
@@ -0,0 +1,54 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MovieInfoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(MoviesInfoController.class)
+@AutoConfigureWebTestClient
+public class MoviesInfoControllerUnitTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private MovieInfoService movieInfoServiceMock;
+
+    private static final String MOVIES_INFO_URL = "/v1/movies-info";
+
+    @Test
+    public void getAllMoviesInfo() {
+
+        var movieinfos = List.of(
+                new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2008, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))
+        );
+
+        when(movieInfoServiceMock.getAllMovieInfo()).thenReturn(Flux.fromIterable(movieinfos));
+
+        webTestClient.get()
+                .uri(MOVIES_INFO_URL)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(MovieInfo.class)
+                .hasSize(3);
+    }
+
+}
