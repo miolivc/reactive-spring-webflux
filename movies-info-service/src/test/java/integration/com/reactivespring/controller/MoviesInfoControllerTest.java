71fb2cea530e8583f2e7ae9b5141b5540b77d6b7
Michelle Oliveira <michelleolivcosta@gmail.com>
2024-09-14 07:32:38 -0300

    caso de teste para stream endpoint utilizando sink

--- movies-info-service/src/test/java/integration/com/reactivespring/controller/MoviesInfoControllerTest.java
+++ movies-info-service/src/test/java/integration/com/reactivespring/controller/MoviesInfoControllerTest.java
@@ -8,9 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -90,6 +92,47 @@ class MoviesInfoControllerTest {
                 .expectBodyList(MovieInfo.class)
                 .hasSize(3);
     }
+
+    @Test
+    void getAllMovieInfo_stream() {
+
+        final MovieInfo movieInfo = MovieInfo.builder()
+                .title("Batman Begins1")
+                .year(2005)
+                .cast(List.of("Christian Bale", "Michael Cane"))
+                .releasedAt(LocalDate.parse("2005-06-15"))
+                .build();
+
+        webTestClient.post()
+                .uri(MOVIES_INFO_URL)
+                .bodyValue(movieInfo)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+                    final MovieInfo responseBody = movieInfoEntityExchangeResult.getResponseBody();
+                    assert responseBody != null;
+                    assertNotNull(responseBody.getId());
+                });
+
+        var movieInfoStreamFlux = webTestClient.get()
+                .uri(MOVIES_INFO_URL + "/stream")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(MovieInfo.class)
+                .getResponseBody();
+
+        StepVerifier.create(movieInfoStreamFlux)
+                .assertNext(movieInfo1 -> {
+                    assert movieInfo1 != null;
+                    assert movieInfo1.getId() != null;
+                })
+                .thenCancel()
+                .verify();
+    }
+
 
     @Test
     void getAllMovieInfoByYear() {
